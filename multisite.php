030d76d097d0e3f2a80ec1f89b1f7e3e023147fc
Vincent Klaiber <vincentklaiber@gmail.com>
2016-04-28 09:39:13 +0200

    Fix multisite plugin

--- multisite.php
+++ multisite.php
@@ -9,47 +9,47 @@
  * file that was distributed with this source code.
  */
 
+/*
+ * Plugin Name: Multisite
+ * Description: A multisite plugin for WordPlate.
+ * Author: WordPlate
+ * Author URI: https://wordplate.github.io
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/wordplate/multisite
+ */
+
 namespace WordPlate\Multisite;
 
-/*
-  * Plugin Name: Multisite
-  * Description: A multisite plugin for WordPlate.
-  * Author: WordPlate
-  * Author URI: https://wordplate.github.io
-  * Version: 1.0.0
-  * Plugin URI: https://github.com/wordplate/multisite
+ /**
+  * Fix network admin URL to include the '/wordpress/' base.
+  *
+  * @see https://core.trac.wordpress.org/ticket/23221
+  *
+  * @param string $url
+  *
+  * @return string
   */
+ function multisite($url)
+ {
+     $urls_to_fix = [
+         'wp-admin',
+         'wp-login.php',
+         'wp-activate.php',
+         'wp-signup.php',
+     ];
 
-/**
- * Fix network admin URL to include the '/wordpress/' base.
- *
- * @see https://core.trac.wordpress.org/ticket/23221
- *
- * @param string $url
- *
- * @return string
- */
-function multisite($url)
-{
-    $urls = [
-        '/wp-admin',
-        '/wp-login.php',
-        '/wp-activate.php',
-        '/wp-signup.php',
-    ];
+     $directory = env('WP_DIR', 'wordpress');
 
-    $directory = env('WP_DIR', 'wordpress');
+     foreach ($urls_to_fix as  $maybe_fix_url) {
+         $fixed_wp_url = '/'.$directory.'/'.$maybe_fix_url;
 
-    foreach ($urls as $url) {
-        $wp_url = '/'.$directory.$url;
+         if (false !== stripos($url,  $maybe_fix_url) && false === stripos($url, $fixed_wp_url)) {
+             $url = str_replace($maybe_fix_url, $fixed_wp_url, $url);
+         }
+     }
 
-        if (false !== stripos($url, $url) && false === stripos($url, $wp_url)) {
-            $url = str_replace($url, $wp_url, $url);
-        }
-    }
-
-    return $url;
-}
+     return $url;
+ }
 
 add_filter('network_site_url', __NAMESPACE__.'\\multisite', 99);
 add_filter('site_url', __NAMESPACE__.'\\multisite', 99);
