37e73b598ea455168e9be4f9f94b3328b3cfc890
Abdus Fauzi <cola.sced@gmail.com>
2017-12-31 18:36:41 +0800

    Removed  / from $fixed_wp_url
    Remove / from $fixed_wp_url because it introduced double forwardslash `//` in the final $url

--- multisite.php
+++ multisite.php
@@ -47,7 +47,7 @@ function multisite(string $url): string
     $directory = env('WP_DIR', 'wordpress');
 
     foreach ($urls_to_fix as  $maybe_fix_url) {
-        $fixed_wp_url = '/'.$directory.'/'.$maybe_fix_url;
+        $fixed_wp_url = $directory.'/'.$maybe_fix_url;
 
         if (false !== stripos($url, $maybe_fix_url) && false === stripos($url, $fixed_wp_url)) {
             $url = str_replace($maybe_fix_url, $fixed_wp_url, $url);
